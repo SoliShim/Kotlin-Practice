548149045efd18d95c3a4805096b449cc11a3441
Sungmin Shim <shimsungmin1221@gmail.com>
2022-04-16 02:10:07 +0900

    added ClassProject

---
+++ ClassProject/app/src/main/java/com/example/classproject/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.classproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        fun t
+
+    }
+}
+
+class Person{
+    lateinit var name : String
+    init{
+        name = "Lionel"
+    }
+    fun process(){
+        name.plus
+    }
+}
